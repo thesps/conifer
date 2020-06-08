5dcd46823dfe4ee08507513a3a5aa3ab397b7900
Sioni Summers <sioni.summers10@imperial.ac.uk>
2020-06-08 13:46:00 +0200

    Add model profiling method

--- conifer/model.py
+++ conifer/model.py
@@ -32,3 +32,28 @@ class model:
 
     def build(self):
         self.backend.build(self.config)
+
+    def profile(self, bins=50, return_data=False, return_figure=True):
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise Exception("matplotlib not found. Please install matplotlib")
+        value = np.array([tree['value'] for trees in self._ensembleDict['trees'] for tree in trees]).flatten()
+        threshold = np.array([tree['threshold'] for trees in self._ensembleDict['trees'] for tree in trees]).flatten()
+        hv, bv = np.histogram(value, bins=bins)
+        wv = bv[1] - bv[0]
+        ht, bt = np.histogram(threshold, bins=bins)
+        wt = bt[1] - bt[0]
+        figure = plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.bar(bv[:-1]+wv/2, hv, width=wv)
+        plt.xlabel('Distribution of tree values (scores)')
+        plt.subplot(1, 2, 2)
+        plt.bar(bt[:-1]+wt/2, ht, width=wt)
+        plt.xlabel('Distribution of tree thresholds')
+        if return_data and return_figure:
+            return (value, threshold, figure)
+        elif return_data:
+            return (value, threshold)
+        elif return_figure:
+            return (figure)
