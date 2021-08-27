2c876050adb9c9a99062856b554980ed377c1d2e
fasif <farheen.asif@cern.ch>
2021-08-27 10:50:22 +0500

    Added pytest file

--- tests/prj_1629782301/prj_1629782303/prj_1629782305/synth.tcl
+++
@@ -1,9 +0,0 @@
-add_files firmware/
-remove_files firmware/SimulationInput.vhd
-remove_files firmware/SimulationOutput.vhd
-remove_files firmware/BDTTestbench.vhd
-remove_files firmware/TestUtil.vhd
-set_property file_type {VHDL 2008} [get_files]
-synth_design -top BDTTop -part XAZU5EV-figd2104-2L-e
-report_utilization -file util.rpt
-write_edif -file my_prj
--- tests/prj_1629782301/prj_1629782303/prj_1629782305/test.tcl
+++
@@ -1,3 +0,0 @@
-vsim -L BDT -L xil_defaultlib xil_defaultlib.testbench
-run 100 ns
-quit -f
