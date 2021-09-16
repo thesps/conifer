ac7af53e36667c346ac4bf5f9720e733a2603863
Sioni Summers <sioni.summers10@imperial.ac.uk>
2021-09-16 17:31:41 +0200

    Add ONNX & multiclass tests

---
+++ tests/test_multiclass.py
@@ -0,0 +1,60 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import GradientBoostingClassifier
+import datetime
+import onnxmltools
+import onnx
+import numpy as np
+import conifer
+import util
+import pytest
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+def train_model():
+    clf = GradientBoostingClassifier(n_estimators=20, learning_rate=1.0,
+                                    max_depth=3, random_state=0).fit(X, y)
+    return clf
+
+skl_model = train_model()
+
+def sklearn_model():
+    return skl_model, skl_model
+
+def onnx_model():
+    from skl2onnx.common.data_types import FloatTensorType
+    initial_type = [('float_input', FloatTensorType([None, X.shape[1]]))]
+    clf = onnxmltools.convert_sklearn(skl_model, 'iris model', initial_types=initial_type)
+    onnx.save(clf, 'iris_bdt.onnx')
+    return clf, 'iris_bdt.onnx'
+
+models = {'sklearn' : sklearn_model,
+          'onnx'    : onnx_model}
+
+predicts = {'sklearn' : util.predict,
+            'onnx'    : util.predict_onnx}
+
+frontends = {'sklearn' : conifer.converters.sklearn,
+            'onnx'    : conifer.converters.onnx}
+
+backends = {'xilinxhls' : conifer.backends.xilinxhls,
+            'vhdl'      : conifer.backends.vhdl}
+
+@pytest.mark.parametrize('frontend', ['sklearn', 'onnx'])
+@pytest.mark.parametrize('backend', ['xilinxhls', 'vhdl'])
+def test_multiclass(frontend, backend):
+    clf, predictor = models[frontend]()
+
+    # Create a conifer config
+    cfg = conifer.backends.xilinxhls.auto_config()
+    # Set the output directory to something unique
+    cfg['OutputDir'] = 'prj_{}'.format(int(datetime.datetime.now().timestamp()))
+    cfg['Precision'] = 'ap_fixed<32,16,AP_RND,AP_SAT>'
+    cfg['XilinxPart'] = 'xcu250-figd2104-2L-e'
+    cnf = conifer.model(clf, frontends[frontend],
+                        backends[backend], cfg)
+    cnf.compile()
+
+    y_hls, y_skl = predicts[frontend](predictor, X, y, cnf)
+    np.testing.assert_allclose(y_hls, y_skl, rtol=1e-2, atol=1e-2)
+    cnf.build()
---
+++ tests/test_onnx_to_hls.py
@@ -0,0 +1,97 @@
+import pytest
+import util
+import onnxmltools
+import onnx
+
+@pytest.fixture
+def train_onnx():
+    # Example BDT creation from: https://scikit-learn.org/stable/modules/ensemble.html
+    from sklearn.datasets import make_hastie_10_2
+    from sklearn.ensemble import GradientBoostingClassifier
+
+    # Make a random dataset from onnxearn 'hastie'
+    X, y = make_hastie_10_2(random_state=0)
+    X_train, X_test = X[:2000], X[2000:]
+    y_train, y_test = y[:2000], y[2000:]
+
+    # Train a BDT
+    clf = GradientBoostingClassifier(n_estimators=20, learning_rate=1.0,
+        max_depth=3, random_state=0).fit(X_train, y_train)
+    
+
+    from skl2onnx.common.data_types import FloatTensorType
+    initial_type = [('float_input', FloatTensorType([None, X_train.shape[1]]))]
+    clf = onnxmltools.convert_sklearn(clf, 'Hastie model', initial_types=initial_type)
+    onnx.save(clf, 'hastie_bdt.onnx')
+
+    return clf, X_test, y_test, 'hastie_bdt.onnx'
+    
+@pytest.fixture
+def hls_convert(train_onnx):
+    import conifer
+    import datetime
+
+    clf, X, y, name = train_onnx
+
+    # Create a conifer config
+    cfg = conifer.backends.vivadohls.auto_config()
+    cfg['Precision'] = 'ap_fixed<32,16,AP_RND,AP_SAT>'
+    # Set the output directory to something unique
+    cfg['OutputDir'] = 'prj_{}'.format(int(datetime.datetime.now().timestamp()))
+    cfg['XilinxPart'] = 'xcu250-figd2104-2L-e'
+
+    # Create and compile the model
+    model = conifer.model(clf, conifer.converters.onnx, conifer.backends.vivadohls, cfg)
+    model.compile()
+    return model
+
+@pytest.fixture
+def vhdl_convert(train_onnx):
+    import conifer
+    import datetime
+
+    clf, X, y, name = train_onnx
+
+    # Create a conifer config
+    cfg = conifer.backends.vivadohls.auto_config()
+    cfg['Precision'] = 'ap_fixed<32,16>'
+    # Set the output directory to something unique
+    cfg['OutputDir'] = 'prj_{}'.format(int(datetime.datetime.now().timestamp()))
+    cfg['XilinxPart'] = 'xcu250-figd2104-2L-e'
+
+    # Create and compile the model
+    model = conifer.model(clf, conifer.converters.onnx, conifer.backends.vhdl, cfg)
+    model.compile()
+    return model
+
+@pytest.fixture
+def predict(train_onnx, hls_convert):
+    clf, X, y, name = train_onnx
+    model = hls_convert
+    return util.predict_onnx(name, X, y, model)
+
+def test_onnx_hls_predict(train_onnx, hls_convert):
+    import numpy as np
+    clf, X, y, name = train_onnx
+    model = hls_convert
+    y_hls, y_onnx = util.predict_onnx(name, X, y, model)
+    assert np.all(np.isclose(y_hls, y_onnx, rtol=1e-2, atol=1e-2))
+
+def test_onnx_build(hls_convert):
+    model = hls_convert
+    model.build()
+    assert True
+
+def test_hdl_predict(train_onnx, vhdl_convert):
+    import numpy as np
+    clf, X, y, name = train_onnx
+    model = vhdl_convert
+    y_hls, y_onnx = util.predict_onnx(name, X, y, model)
+    assert np.all(np.isclose(y_hls, y_onnx, rtol=1e-2, atol=1e-2))
+
+def test_hdl_build(vhdl_convert):
+    model = vhdl_convert
+    model.build()
+    assert True
+
+
