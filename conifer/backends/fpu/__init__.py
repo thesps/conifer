aaf0a0997ab6feb9e029641eff24bac85c7ce9b5
Sioni Summers <14807534+thesps@users.noreply.github.com>
2023-01-16 15:01:58 +0100

    Rename fpu_driver to runtime

--- conifer/backends/fpu/__init__.py
+++ conifer/backends/fpu/__init__.py
@@ -2,8 +2,8 @@ import logging
 logger = logging.getLogger(__name__)
 
 try:
-    from conifer.backends.fpu.fpu_driver import ZynqDriver
+    from conifer.backends.fpu import runtime
 except ImportError:
     ZynqDriver = None
-    logger.warn('FPUDriver could not be imported. Interacting with FPUs will not be possible.')
+    logger.warn('runtime module could not be imported. Interacting with FPUs will not be possible.')
 from conifer.backends.fpu.writer import make_model, FPUModel, auto_config, FPUBuilder, ZynqFPUBuilder, AlveoFPUBuilder, FPUConfig
