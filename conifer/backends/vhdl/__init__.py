45daefa937c0c78ffe50df8adefd2b8eba0b50fa
Sioni Summers <14807534+thesps@users.noreply.github.com>
2023-01-23 17:03:35 +0100

    Add simulator discovery to VHDL backend

--- conifer/backends/vhdl/__init__.py
+++ conifer/backends/vhdl/__init__.py
@@ -1,3 +1,11 @@
 from conifer.backends.vhdl.writer import make_model, auto_config
 from conifer.backends.vhdl.simulators import Modelsim, GHDL, Xsim
 simulator = Xsim
+import logging
+logger = logging.getLogger(__name__)
+for sim in [Xsim, GHDL, Modelsim]:
+  from conifer.backends.vhdl.simulators import _touch
+  if _touch(sim):
+    logger.info(f'Found {sim.__name__}, setting VHDL simulator to {sim.__name__}')
+    simulator = sim
+    break
