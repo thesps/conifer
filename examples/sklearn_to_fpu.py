e2cd85104e721e749c3ddb970b7d06d1aff5c1ed
Sioni Summers <14807534+thesps@users.noreply.github.com>
2023-02-21 16:52:14 +0100

    Update FPU example

--- examples/sklearn_to_fpu.py
+++ examples/sklearn_to_fpu.py
@@ -15,41 +15,38 @@ import numpy as np
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+# First, either build an FPU yourself (see build_fpu_alveo.py and build_fpu_pynq.py)
+# Or download one from the conifer website for your device.
+# Source the Xilinx RunTime environment
+# Then, load the FPU onto Alveo (change the path to point to your xclbin file)
+device = conifer.backends.fpu.runtime.AlveoDriver('fpu-u200_100TE_512N_DS/fpu.xclbin')
+print(device.config)
+
 # Make a random dataset from sklearn 'hastie'
-X, y = make_hastie_10_2(random_state=0)#, n_samples=500_000)
+X, y = make_hastie_10_2(random_state=0, n_samples=1_000)
+X = X.astype('float32') # for FPU compatibility
 
 # Train a BDT
-clf = GradientBoostingClassifier(n_estimators=50, learning_rate=1.0,
-                                 max_depth=3, random_state=0).fit(X, y)
+clf = GradientBoostingClassifier(n_estimators=32, learning_rate=1.0,
+                                 max_depth=5, random_state=0).fit(X, y)
 
 stamp = int(datetime.datetime.now().timestamp())
 # Create a conifer config
 cfg = conifer.backends.fpu.auto_config()
-# Set the output directory to something unique
-cfg['OutputDir'] = 'prj_fpu_{}'.format(stamp)
-# set the target FPU config to match the one downloaded
-with open('fpu.json') as f:
-  cfg['FPU'] = json.load(f)
 
-# Create and compile the model
+# Important! Set the target FPU config to match the one loaded on the board
+cfg['FPU'] = device.config
+
+# Create and compile the model for target FPU
 model = conifer.converters.convert_from_sklearn(clf, cfg)
-model.write()
-y_clf = clf.decision_function(X)
-np.save(f'{cfg["OutputDir"]}/X.npy', X.astype('float32'))
-np.save(f'{cfg["OutputDir"]}/y.npy', y.astype('float32'))
 
-# to run inference on a pynq-z2, copy the driver conifer/backends/fpu/runtime.py to the pynq,
-# as well as the .zip archive containing the FPU binary,
-# and the nodes.json, my-prj.json saved by the model.write() above,
-# and the X.npy, y.npy reference data
-# inference currently works like:
-# import json
-# from runtime import ZynqDriver
-# import numpy as np
-# fpu = ZynqDriver('fpu.bit')
-# print(fpu.get_info())
-# nodes = json.load(open('nodes.json'))
-# model = json.load(open('my-prj.json'))
-# X = np.load('X.npy')
-# fpu.load(nodes['nodes'], nodes['scales'], model['n_features'], model['n_classes'], batch_size=X.shape[0])
-# y_fpu = fpu.predict(X)
+# Load this model onto the FPU
+# Important! Set the batch size to allocate buffers
+# Inference is more efficient with larger batches
+model.attach_device(device, batch_size=X.shape[0])
+
+# Execute model predictions on FPU
+y_fpu = model.decision_function(X)
+
+# Execute model prediction on sklearn
+y_skl = clf.decision_function(X)
