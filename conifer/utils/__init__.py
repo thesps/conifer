65d247f3fd5083167b7b203686b64dc68187b2a8
Sioni Summers <14807534+thesps@users.noreply.github.com>
2023-08-09 15:40:17 +0200

    Make import of modelling module optional

--- conifer/utils/__init__.py
+++ conifer/utils/__init__.py
@@ -1,4 +1,7 @@
 from .fixed_point import FixedPointConverter
 from conifer.utils.misc import _ap_include, _json_include, _gcc_opts, _py_executable, copydocstring
 from conifer.utils.estimation import performance_estimates
-from conifer.utils.modelling import run_scan, gather_reports, do_fits
+try:
+  from conifer.utils.modelling import run_scan, gather_reports, do_fits
+except ImportError:
+   modelling = None
