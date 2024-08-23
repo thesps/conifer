869944fd0909a1822481599b4ac74ef52af4d14c
Piero Viscone <piero.viscone@cern.ch>
2024-08-23 23:39:52 +0200

    added library and splitting_convention to _ensemble_fields

--- conifer/model.py
+++ conifer/model.py
@@ -186,9 +186,7 @@ class ModelBase:
     Primary interface to write, compile, execute, and synthesize conifer projects
     '''
 
-    _ensemble_fields = ['n_classes', 'n_features', 'n_trees', 'max_depth', 'init_predict', 'norm', 'feature_map']
-
-    _optional_fields = []
+    _ensemble_fields = ['n_classes', 'n_features', 'n_trees', 'max_depth', 'init_predict', 'norm', "library", "splitting_convention"]
 
     def __init__(self, ensembleDict, configDict=None, metadata=None):
         if 'feature_map' not in ensembleDict:
@@ -197,9 +195,7 @@ class ModelBase:
             val = ensembleDict.get(key, None)
             assert val is not None, f'Missing expected key {key} in ensembleDict'
             setattr(self, key, val)
-        for key in ModelBase._optional_fields:
-            val = ensembleDict.get(key, None)
-            setattr(self, key, val)
+        assert self.splitting_convention in ["<", "<="]
         trees = ensembleDict.get('trees', None)
         assert trees is not None, f'Missing expected key trees in ensembleDict'
         self.trees = [[DecisionTreeBase(treeDict) for treeDict in trees_class] for trees_class in trees]
