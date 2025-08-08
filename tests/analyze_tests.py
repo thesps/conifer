c8d20214a3e2bd3b92c852419bcf4269d22d7835
Sioni Summers <14807534+thesps@users.noreply.github.com>
2025-08-08 21:16:52 +0200

    override extends and allow some jobs to fail

--- tests/analyze_tests.py
+++ tests/analyze_tests.py
@@ -6,7 +6,14 @@ pytest.{name}:
   extends: .pytest-{extends}
   variables:
     PYTESTFILE: {test_file}
+  allow_failure: {allow_failure}
 '''
+
+# override the auto detection of which script to extend for the following jobs
+extends_override = {'backends' : 'fpga'}
+
+# allow the following jobs to fail
+allow_failure = ['backends', 'xgb_converter', 'onnx_to_hls']
 
 # check whether "build" method is called in the test -> needs different resources
 def calls_build(test_filename):
@@ -26,9 +33,13 @@ def generate_test_yaml(directory='.'):
         name = file_name.replace('test_', '').replace('.py', '')
         build = calls_build(test_file)
         extends = 'fpga' if build else 'plain'
+        if name in extends_override.keys():
+            extends = extends_override[name]
+        allow_fail = 'True' if name in allow_failure else 'False'
         test_yml = yaml.safe_load(template.format(name=name,
                                                   extends=extends,
-                                                  test_file=test_file))
+                                                  test_file=test_file,
+                                                  allow_failure=allow_fail))
         if yml is None:
             yml = test_yml
         else:
