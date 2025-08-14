e0eade8d67f5ffbfa0ad47797c7f413725c33adb
Sioni Summers <14807534+thesps@users.noreply.github.com>
2025-08-14 14:46:31 +0200

    Cause tests to fail when errors are logged in logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,9 @@
 import pytest
+import logging
 from tests.util import train_skl, hls_convert, vhdl_convert, predict
+
+@pytest.fixture(autouse=True)
+def no_logs_gte_error(caplog):
+    yield
+    errors = [record for record in caplog.get_records('call') if record.levelno >= logging.ERROR]
+    assert not errors
