b2f7b75e908a6d4b1b129063f2ac5a6172d19955
Sioni Summers <14807534+thesps@users.noreply.github.com>
2025-11-13 14:40:20 +0100

    First pass pyxrt backend for Alveo HLS driver

--- conifer/backends/xilinxhls/runtime.py
+++ conifer/backends/xilinxhls/runtime.py
@@ -1,4 +1,9 @@
+import logging
+logger = logging.getLogger(__name__)
+from typing import Union
 import pynq
+import pyxrt
+import numpy as np
 
 class ZynqDriver:
   def __init__(self, bitfile, ip_name=None, batch_size=1):
@@ -55,7 +60,7 @@ class ZynqDriver:
       pass
     return self.ybuf
   
-class AlveoDriver:
+class PynqAlveoDriver:
   def __init__(self, bitfile, ip_name=None, batch_size=1):
     self.overlay = pynq.Overlay(bitfile)
     ips = [at for at in dir(self.overlay) if 'conifer' in at]
@@ -110,3 +115,96 @@ class AlveoDriver:
       pass
     self.ybuf.sync_from_device()
     return self.ybuf
+
+class PyXrtAlveoDriver:
+  def __init__(self,
+               xclbin : Union[str, pyxrt.xclbin],
+               device : Union[int, pyxrt.device],
+               ip_name=None,
+               n_features=None,
+               n_classes=None,
+               batch_size=1):
+    
+    # load the device
+    if isinstance(device, int):
+      self.device = pyxrt.device(device)
+    elif isinstance(device, pyxrt.device):
+      self.device = device
+    else:
+      logger.error(f"Can't handle device '{device}' (type {type(device)})")
+
+    # load the xclbin
+    if isinstance(xclbin, str):
+      self.xclbin = pyxrt.xclbin(xclbin)
+    elif isinstance(xclbin, pyxrt.xclbin):
+      self.xclbin = xclbin
+    else:
+      logger.error(f"Can't handle xclbin '{xclbin}' (type {type(xclbin)})")
+
+    logger.debug('Loading xclbin onto device')
+    uuid = self.device.load_xclbin(xclbin)
+
+    kernels = self.xclbin.get_kernels()
+    logger.debug(f'Kernels in xclbin: {kernels}')
+    assert len(kernels) == 1, f"Can only handle 1 kernel in xclbin, found {len(kernels)}"
+    kernel = kernels[0]
+
+    self.kernel = pyxrt.kernel(self.device, uuid, kernel.get_name(), pyxrt.kernel.cu_access_mode.exclusive)
+
+    # TODO
+    # Until register reading is added, need to specify the n_features and n_classes
+    self.n_features = n_features
+    self.n_classes = n_classes
+    self._init_buffers(batch_size)
+
+  def _init_Xy_buffers(self, X_shape, y_shape, x_dtype='float32', y_dtype='float32'):
+    Xsize = np.prod(X_shape) * np.dtype(x_dtype).itemsize
+    ysize = np.prod(y_shape) * np.dtype(y_dtype).itemsize
+    self.XbufHandle = pyxrt.bo(self.device, Xsize, pyxrt.bo.normal, self.kernel.group_id(3))
+    self.ybufHandle = pyxrt.bo(self.device, ysize, pyxrt.bo.normal, self.kernel.group_id(3))
+    self.Xbuf = self.XbufHandle.map()
+    self.ybuf = self.ybufHandle.map()
+    self.X_dtype = x_dtype
+    self.y_dtype = y_dtype
+
+  def _init_buffers(self, batch_size=1, x_dtype='float32', y_dtype='float32'):
+    X_shape = (batch_size, self.n_features)
+    y_shape = (batch_size, self.n_classes)
+    self._init_Xy_buffers(X_shape=X_shape, y_shape=y_shape, x_dtype=x_dtype, y_dtype=y_dtype)
+
+  def decision_function(self, X):
+    '''
+    Execute inference
+
+    Parameters
+    ----------
+    X: ndarray of shape (batch_size, n_features), dtype float32
+      Input sample. Shape must match allocated buffers
+
+    Returns
+    ----------
+    score: ndarray of shape (batch_size, n_classes)
+    '''
+    assert X.ndim == 2, "Expected 2D inputs."
+    assert np.prod(X.shape) * X.dtype.itemsize == self.Xbuf.nbytes, f"Cannot copy {np.prod(X.shape) * X.dtype.itemsize} bytes data into {self.Xbuf.nbytes} buffer"
+    assert X.dtype == np.dtype(self.X_dtype), f"Got X dtype {X.dtype}, expected {self.X_dtype}"
+    return self._decision_function(X)
+  
+  def _decision_function(self, X):
+    '''
+    Execute inference without checks on X shape and dtype
+
+    Parameters
+    ----------
+    X: ndarray
+
+    Returns
+    ----------
+    score: ndarray
+    '''
+    self.XbufHandle.write(X.tobytes(), 0)
+    self.XbufHandle.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_TO_DEVICE)
+    run = self.kernel(X.shape[0], 0, 0, self.XbufHandle, self.ybufHandle)
+    run.wait()
+    self.ybufHandle.sync(pyxrt.xclBOSyncDirection.XCL_BO_SYNC_BO_FROM_DEVICE)
+    return np.frombuffer(self.ybuf, dtype=self.y_dtype)
