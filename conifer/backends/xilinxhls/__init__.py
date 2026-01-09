3df4f0f9c6e36949fd6be3f049bb34acd703c4e1
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-01-09 16:49:34 +0100

    Add conifer_xrt_runtime package, and its pyproject toml requirements. Move to pyproject.toml for main conifer. Use conifer_xrt_runtime package in the conifer HLS backend driver

--- conifer/backends/xilinxhls/__init__.py
+++ conifer/backends/xilinxhls/__init__.py
@@ -2,9 +2,4 @@ import logging
 logger = logging.getLogger(__name__)
 
 from conifer.backends.xilinxhls.writer import make_model, auto_config
-try:
-    from conifer.backends.xilinxhls import runtime
-except ImportError:
-    ZynqDriver = None
-    AlveoDriver = None
-    logger.warn('runtime module could not be imported. Interacting with accelerators will not be possible.')
+from conifer.backends.xilinxhls.runtime import ZynqDriver, PynqAlveoDriver, XrtDriver
--- conifer/backends/xilinxhls/runtime.py
+++ conifer/backends/xilinxhls/runtime.py
@@ -1,4 +1,5 @@
 import pynq
+import numpy as np
 
 class ZynqDriver:
   def __init__(self, bitfile, ip_name=None, batch_size=1):
@@ -110,3 +111,27 @@ class AlveoDriver:
       pass
     self.ybuf.sync_from_device()
     return self.ybuf
+
+class XrtAlveoDriver:
+
+  def __init__(self, xclbin_path, kernel_name, device_index=0, batch_size=1):
+    self.device = ConiferXilinxHLSXRTRuntime(device_index, xclbin_path, kernel_name)
+    self.device.allocate_buffers(batch_size)
+
+  def allocate_buffers(self, batch_size=1):
+    self.device.allocate_buffers(batch_size)
+
+  def decision_function(self, X: np.ndarray) -> np.ndarray:
+    '''
+    Execute inference
+
+    Parameters
+    ----------
+    X: ndarray of shape (batch_size, n_features), dtype float32
+      Input sample. Shape must match allocated buffers
+
+    Returns
+    ----------
+    score: ndarray of shape (batch_size, n_classes)
+    '''
+    return self.device.decision_function(X)
