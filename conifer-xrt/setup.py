099fcdbd2fdff2cb5d1833fe8cd6b2c99c5f521c
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-01-09 16:49:34 +0100

    Add conifer_xrt_runtime package, and its pyproject toml requirements. Move to pyproject.toml for main conifer. Use conifer_xrt_runtime package in the conifer HLS backend driver

---
+++ conifer-xrt/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+from glob import glob
+import os
+
+XRT = os.environ.get("XILINX_XRT")
+if XRT is None:
+    raise RuntimeError(
+        "XILINX_XRT environment variable must be set to build conifer-xrt"
+    )
+
+ext_modules = [
+    Pybind11Extension(
+        "conifer_xrt_runtime",
+        sorted(glob("src/*.cpp")),
+        include_dirs=[XRT + "/include"],
+        extra_compile_args=["-std=c++17"],
+        extra_objects=[XRT + "/lib/libxrt_coreutil.so"],
+    )
+]
+
+setup(ext_modules=ext_modules)
