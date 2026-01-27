ada5ad1ad02f385762beb3d0f425200c52280b64
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-01-27 13:35:07 +0100

    Make sure all external libraries are included in the package data. Update the logic of finding which headers to use based on the externals
    Tidy up hls_stream include, and remove setup of JSON and ap_types headers from CI before_script
    
    Make sure submodules are cloned in CI

--- conifer/utils/misc.py
+++ conifer/utils/misc.py
@@ -1,36 +1,46 @@
 import os
 import sys
 import logging
+import conifer
 logger = logging.getLogger(__name__)
 
 def _ap_include():
   '''
-  Get the -I include option for the g++ compile for the ap_ headers.
-  If set use XILINX_AP_INCLUDE, then check for Vivado/Vitis HLS
+  Get the -I include option for the g++ compile for the ap_ and hls_stream headers.
+  Default to the conifer bundled headers, but check for XILINX environment variables
+  to override.
   '''
-  ret = None
+  ap_types_path = f'{conifer.__path__[0]}/external/ap_types/include'
+  hls_stream_path = f'{conifer.__path__[0]}/external/hls/'
   variables = [('XILINX_AP_INCLUDE', ''),
                ('XILINX_HLS', '/include'),
                ('XILINX_VIVADO', '/include')]
   for var, include in variables:
     if os.environ.get(var) is not None and os.path.isdir(f'{os.environ.get(var)}/{include}'):
-      ret = f'-I{os.environ.get(var)}/{include}'
-      logger.debug(f'Including ap_ headers from {var}: {ret}')
+      ap_types_path = f'{os.environ.get(var)}/{include}'
+      if os.path.exists(f'{os.environ.get(var)}/include/hls_stream.h'):
+        hls_stream_path = '' # no need to add hls_stream path again with Vitis/Vivado install
       break
-  if ret is None:
-    logger.warn(f'Could not find ap_ headers (e.g., ap_fixed.h). None of {", ".join([var[0] for var in variables])} are defined')
+  if not os.path.isdir(ap_types_path):
+    logger.warning(f'Could not find ap_ headers (e.g., ap_fixed.h) at {ap_types_path}')
+  logger.debug(f'Including ap_ headers from {ap_types_path}')
+
+  if not os.path.isdir(hls_stream_path):
+    logger.warning(f'Could not find hls_stream headers at {hls_stream_path}')
+  logger.debug(f'Including hls_stream headers from: {hls_stream_path}')
+
+  ret = f'-I{ap_types_path}' + (f' -I{hls_stream_path}' if hls_stream_path != '' else '')
   return ret
 
 def _json_include():
   '''
   Get the -I include option for the g++ compile for JSON headers.
   '''
-  if os.environ.get("JSON_ROOT") is not None:
-    ret = f'-I{os.environ.get("JSON_ROOT")}'
-    logger.debug(f'Include JSON headers from JSON_ROOT: {ret}')
-  else:
-    ret = None
-    logger.warn('Could not find JSON headers. JSON_ROOT not defined')
+  json_path = f'{conifer.__path__[0]}/external/json/'
+  if not os.path.isdir(json_path):
+    logger.warning(f'Could not find JSON headers at expected location: {json_path}')
+  ret = f'-I{json_path}'
+  logger.debug(f'Including JSON headers from: {json_path}')
   return ret
 
 def _gcc_opts():
